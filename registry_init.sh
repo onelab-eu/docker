3f37f1a03248c2ced4b078471177bfd747e91f8a
radomirklacza <radomirklacza@users.noreply.github.com>
2016-10-07 12:14:40 +0200

    Updating init script for docker

--- registry_init.sh
+++ registry_init.sh
@@ -1,3 +1,6 @@
 sfaadmin.py reg import_registry
 sfaadmin.py reg register -t user -x onelab.myslice -e support@myslice.info -k /var/myslice/myslice.pub
 sfaadmin.py reg update -t authority -x onelab -p onelab.myslice
+mkdir /etc/sfa/root_certificate
+cd /etc/sfa/root_certificate
+sfaadmin.py cert export onelab
