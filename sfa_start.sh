4f2c473e92e44dab837d38970b7ec99530093148
radomirklacza <radomirklacza@users.noreply.github.com>
2016-10-07 17:19:18 +0200

    Upgrading SFA start script

--- sfa_start.sh
+++ sfa_start.sh
@@ -1,1 +1,11 @@
 # This script is to start SFA services 
+
+#As deamon 
+/usr/bin/sfa-start.py -t -d
+#As deamon 
+/usr/bin/sfa-start.py -r -d 
+# As deamon 
+/usr/bin/sfa-start.py -a -d
+# Read log
+tail -f /var/log/sfa.log
+
