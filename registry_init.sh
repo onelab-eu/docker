bb15c24415d677e29f12488f4703d801156ee7a7
radomirklacza <radomirklacza@users.noreply.github.com>
2016-10-10 10:59:55 +0200

    Adding first initialization

--- registry_init.sh
+++ registry_init.sh
@@ -5,39 +5,49 @@
 TEMP=`getopt -o e:p:r: --long email:,password:,root-authority: -n 'registry_init.sh' -- "$@"`
 eval set -- "$TEMP"
 
-while true ; do
-    case "$1" in
-        -e|--email)
-            case "$2" in
-                "") shift 2 ;;
-                *) ADMIN_EMAIL=$2 ; shift 2 ;;
-            esac ;;
-        -p|--password)
-            case "$2" in
-                "") shift 2 ;;
-                *) ADMIN_PASSWORD=$2 ; shift 2 ;;
-            esac ;;
-        -r|--root-authority)
-            case "$2" in
-                "") shift 2 ;;
-                *) ROOT_AUTHORITY=$2 ; shift 2 ;;
-            esac ;;
-        --) shift ; break ;;
-        *) echo "Internal error!" ; exit 1 ;;
-    esac
-done
-ROOT_AUT=$ROOT_AUTHORITY
-ROOT_AUTHORITY=$ROOT_AUTHORITY".myslice"
+FILE=/root/docker/initialized
 
-echo "pass = $ADMIN_PASSWORD"
-echo "email = $ADMIN_EMAIL"
-echo "auth = $ROOT_AUTHORITY"
-echo "root = $ROOT_AUT"
+if [ -f $FILE ];
+then
+   echo "SFA was already initialized, to reinitialize it (experimental), please remove file /root/docker/initialized"
+else
+   echo "Initializing SFA"
+    while true ; do
+        case "$1" in
+            -e|--email)
+                case "$2" in
+                    "") shift 2 ;;
+                    *) ADMIN_EMAIL=$2 ; shift 2 ;;
+                esac ;;
+            -p|--password)
+                case "$2" in
+                    "") shift 2 ;;
+                    *) ADMIN_PASSWORD=$2 ; shift 2 ;;
+                esac ;;
+            -r|--root-authority)
+                case "$2" in
+                    "") shift 2 ;;
+                    *) ROOT_AUTHORITY=$2 ; shift 2 ;;
+                esac ;;
+            --) shift ; break ;;
+            *) echo "Internal error!" ; exit 1 ;;
+        esac
+    done
+    ROOT_AUT=$ROOT_AUTHORITY
+    ROOT_AUTHORITY=$ROOT_AUTHORITY".myslice"
 
-sfaadmin.py reg import_registry
-sfaadmin.py reg register -t user -x $ROOT_AUTHORITY -e $ADMIN_EMAIL -k /var/myslice/myslice.pub
-sfaadmin.py reg update -t authority -x $ROOT_AUT -p $ROOT_AUTHORITY
-mkdir /etc/sfa/root_certificate
-cd /etc/sfa/root_certificate
-sfaadmin.py cert export onelab
+    echo "pass = $ADMIN_PASSWORD"
+    echo "email = $ADMIN_EMAIL"
+    echo "auth = $ROOT_AUTHORITY"
+    echo "root = $ROOT_AUT"
+
+    sfaadmin.py reg import_registry
+    sfaadmin.py reg register -t user -x $ROOT_AUTHORITY -e $ADMIN_EMAIL -k /var/myslice/myslice.pub
+    sfaadmin.py reg update -t authority -x $ROOT_AUT -p $ROOT_AUTHORITY
+    mkdir /etc/sfa/root_certificate
+    cd /etc/sfa/root_certificate
+    sfaadmin.py cert export onelab
+    touch /root/docker/initialized
+    echo "SFA was already initialized, to reinitialize it, pls remove this file" > /root/docker/initialized
+fi
 
