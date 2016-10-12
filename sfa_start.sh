6cf3fa8606e03b28f291126eb04f50a4812a3f21
radomirklacza <radomirklacza@users.noreply.github.com>
2016-10-12 12:15:13 +0200

    Update sfa_start.sh

--- sfa_start.sh
+++ sfa_start.sh
@@ -14,7 +14,7 @@ if [ -f $FILE ];
 then
    echo "SFA was already initialized, to reinitialize it (experimental), please remove file /root/docker/initialized"
 else
-   sh /root/docker/registry_init.sh -e "admins@onelab.eu" -p "test_password" -r onelab
+   sh /root/docker/registry_init.sh -e $ADMIN_EMAIL -p $ADMIN_PASSWORD -r $ROOT_AUTHORITY
    killall5 -9 sfa-start.py
    /usr/bin/sfa-start.py -t -d
    /usr/bin/sfa-start.py -r -d 
