b6cffa6f68ee7de600333ed7030d5136842f6546
radomirklacza <radomirklacza@users.noreply.github.com>
2016-10-12 17:09:34 +0200

    Update web_start.sh

--- web_start.sh
+++ web_start.sh
@@ -1,1 +1,31 @@
+# This scripts starts the
+FILE=/root/docker/web_initialized
 
+if [ -f $FILE ];
+then
+   echo "WEB was already initialized, to reinitialize it (experimental), please remove file /root/docker/web_initialized"
+   #As deamon 
+   /home/radomir/myslice/myslice/bin/myslice-live &
+   #As deamon 
+   /home/radomir/myslice/myslice/bin/myslice-monitor &
+   # As deamon 
+   /home/radomir/myslice/myslice/bin/myslice-server &
+   #As deamon
+   /home/radomir/myslice/myslice/bin/myslice-web &
+
+else
+   cd /var/myslice
+   cp /etc/sfa/root_certificate/myslice.cert myslice.cert
+   cp /etc/sfa/root_certificate/myslice.pkey myslice.pkey
+   #STart 
+   /home/radomir/myslice/myslice/bin/myslice-live &
+   #As deamon 
+   /home/radomir/myslice/myslice/bin/myslice-monitor &
+   # As deamon 
+   /home/radomir/myslice/myslice/bin/myslice-server &
+   #As deamon
+   /home/radomir/myslice/myslice/bin/myslice-web &
+
+fi 
+
+/bin/bash
